2ecdc8a842732fd4d4712d424cb6cef46079e97a
Sam Gitonga <samggitonga@gmail.com>
2023-06-12 19:50:29 +0300

    Longest common prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+
+        prefix = ''
+        for word in strs:
+            for character in word:
+                if len(prefix) > 0:
+
+                else:
+                    prefix =
